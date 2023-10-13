44b3dfcf5f7748fb9b688c9e571124a513aa225c
octavi42 <53470992+octavi42@users.noreply.github.com>
2023-10-13 18:58:20 +0300

    first grades methods

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,106 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    public class Student {
+        private String name;
+        private int grade;
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        public Student(String name, int grade) {
+            this.name = name;
+            this.grade = grade;
         }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public double getGrade() {
+            return grade;
+        }
+
+        public void setGrade(int grade) {
+            this.grade = grade;
+        }
+
+        @Override
+        public String toString() {
+            return "Student [name=" + name + ", grade=" + grade + "]";
+        }
+    }
+
+
+    // method that gets an array and returns the grades that are lower than 38
+    static int[] low_grades(int[] noten) {
+        int [] final_grades = new int[noten.length];
+        for (int i = 0; i < noten.length; i++) {
+            if (noten[i] < 38) {
+                final_grades[i] = noten[i];
+            }
+        }
+
+        return final_grades;
+    }
+
+
+    // a method that returns the average grade
+    static double average_grade(int[] noten) {
+        int sum = 0;
+        for (int i = 0; i < noten.length; i++) {
+            sum += noten[i];
+        }
+
+        return (double) sum / noten.length;
+    }
+
+    // method that returns the rounded grades
+    static int[] rounded_grades(int[] noten) {
+        int[] rounded_grades = new int[noten.length];
+        for(int i = 0; i < noten.length; i++) {
+
+            // find the next multiple of 5 ot the grade
+            int next_multiple = (int) (noten[i] / 5) * 5;
+            if (noten[i] - next_multiple < 3) {
+                rounded_grades[i] = next_multiple;
+            } else {
+                rounded_grades[i] = noten[i];
+            }
+        }
+
+        return rounded_grades;
+    }
+
+
+    // method that returns the maximal rounded grades
+    static int maximal_grade(int[] noten) {
+        noten = rounded_grades(noten);
+        int max_grade = noten[0];
+
+        for (int i = 1; i < noten.length; i++) {
+            if (noten[i] > max_grade) {
+                max_grade = noten[i];
+            }
+        }
+
+        return max_grade;
+    }
+
+
+    public static void main(String[] args) {
+        int[] noten = {10, 30, 29, 33, 50, 84};
+
+        System.out.println(Arrays.toString(low_grades(noten)));
+        System.out.println(average_grade(noten));
+        System.out.println(Arrays.toString(rounded_grades(noten)));
+        System.out.println(maximal_grade(noten));
+
+
     }
 }
